70fa9b2c6b8dc64ae3556885ca043fb50e67e5de
Umran <mohammedumran.0416@gmail.com>
2025-09-19 19:48:18 +0100

    prepping for useState

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,17 @@
 export default function Main() {
+
+    const ingredients = ['Chicken','Tomato paste', 'Pasta']
+
+    const ingredientsList = ingredients.map(ingredient => {
+        <li key={ingredient}>{ingredient}</li>
+    })
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get('ingredient')
+
+    }
     return (
         <main>
             <form className="add-ingredient-form">
@@ -6,6 +19,7 @@ export default function Main() {
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
